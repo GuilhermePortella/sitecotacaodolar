7bad3cad0fc619631df8879e831b8e599e74490e
guilhermeportella <guilhermeportella2@gmail.com>
2022-01-08 16:57:37 -0300

    Atualizacao

--- src/app/components/creators/creators.component.ts
+++ src/app/components/creators/creators.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-creators',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  cancel(): void {
+    this.router.navigate(['home'])
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute ) { }
+
 
   ngOnInit(): void {
+  }
+
+  creatorsOption(): void {
+    this.router.navigate(['creators'])
   }
 
 }
