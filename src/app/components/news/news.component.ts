2b2768c03bfeb074be71011c7db0bb8366cf27bf
guilhermeportella <guilhermeportella2@gmail.com>
2022-01-22 18:04:52 -0300

    Inicio pag de noticias

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
+
+  cancel(): void {
+    this.router.navigate(['home'])
+  }
+
 
 }
